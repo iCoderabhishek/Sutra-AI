0be9ff89cc340376bf94919861791fb5cfd8dc67
Abhishek Jha <iamabhishek1310@gmail.com>
2026-09-09 20:38:22 +0530

    Chore: adding the libs as they were hidden by gitignore before

---
+++ apps/backend/src/libs/env.ts
@@ -0,0 +1,5 @@
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string
+export const GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL as string
+export const DATABASE_URL = process.env.DATABASE_URL as string;
+export const SESSION_SECRET = process.env.SESSION_SECRET as string
