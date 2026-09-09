72943bf8062ac43b3a12f3bed06826cd79b61c22
Abhishek Jha <iamabhishek1310@gmail.com>
2026-09-09 20:37:33 +0530

    Initialise cookie sesion

--- apps/backend/src/api/server.ts
+++ apps/backend/src/api/server.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from "dotenv"
 import authRoutes from '../modules/auth/routes'
+import cookieSession from "cookie-session"
+import { SESSION_SECRET } from '../lib/env'
 
 dotenv.config()
 const app = express()
@@ -13,7 +15,13 @@ app.get("/", (req, res) => {
         "message": "I am alive"
     })
 })
-
+app.use(cookieSession({
+    name: 'session',
+    keys: [SESSION_SECRET],
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 app.use("/api/v1/auth", authRoutes)
 
 
