edd42794c1d47f8f592d73069168ceb0c1567772
Abhishek Jha <iamabhishek1310@gmail.com>
2026-09-10 13:22:17 +0530

    Adding few more checks for crashguard

--- apps/backend/src/libs/queue.ts
+++ apps/backend/src/libs/queue.ts
@@ -4,7 +4,10 @@ import redis from "./redis";
 export const agentQueue = new Queue("agent-queue", { connection: redis });
 
 export const worker = new Worker("agent-queue", async (job) => {
-    const { agentId } = job.data;
-    console.log(`{worker} Processing agent ${agentId}`);
-
+    try {
+        const { agentId } = job.data;
+        console.log(`{worker} Processing agent ${agentId}`);
+    } catch (error) {
+        console.log(`{worker} Error processing agent ${job.data.agentId}`, error);
+    }
 }, { connection: redis });
--- apps/backend/src/libs/redis.ts
+++ apps/backend/src/libs/redis.ts
@@ -1,6 +1,8 @@
 import { REDIS_URL } from "./env";
 import { Redis } from "ioredis"
 
-const redis = new Redis(REDIS_URL)
+const redis = new Redis(REDIS_URL, {
+    maxRetriesPerRequest: 0
+})
 
 export default redis
